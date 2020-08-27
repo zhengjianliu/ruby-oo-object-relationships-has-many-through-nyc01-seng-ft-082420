12a97f9b38de4bc46c3f6ab086eb02e0e8b88f77
zhengjianliu <comezheng@gmail.com>
2020-08-27 18:54:33 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Customer
-  
+    attr_accessor :name, :age
+    @@all =[]
+    def initialize (name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    def meals
+        Meal.all.select{ |meal|
+            meal.customer == self
+        }
+    end
+
+    def waiters
+        Meal.all.find_all{ |meal|
+            meal.customer == self 
+    }.map{ |meal| meal.waiter}
+    
+    end
+
+    def new_meal(current_waiter, current_total, current_tip)
+        Meal.new(current_waiter,self,current_total,current_tip)
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Waiter
+    attr_accessor :name, :age
+    @@all =[]
+    def initialize (name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(current_customer, current_total, current_tip)
+        Meal.new(self, current_customer,current_total, current_tip)
+    end
+
+    def meals
+        Meal.all.filter{ |meal|
+            meal.waiter == self
+        }
+    end
+
+    def best_tipper
+        max = 0
+        best_customer = nil
+        Meal.all.each{ |meal|
+            if meal.tip > max
+                max = meal.tip
+                best_customer = meal.customer
+            else
+                max = max
+                best_customer = best_customer
+            end
+
+        }
+        best_customer
+    end
+
+
+
 
 end
